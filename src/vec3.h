8ae9cb6e1cceca8708c634ef7fc997d9f1464539
박창선 <>
2024-04-07 15:58:07 +0900

    bouncing

--- src/vec3.h
+++ src/vec3.h
@@ -10,9 +10,6 @@ class vec3 {
 public:
     double e[3];
 
-//    vec3() : e{0,0,0} {}
-//    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
-
     vec3() {
         e[0] = e[1] = e[2] = 0;
     }
@@ -21,6 +18,14 @@ public:
         e[0] = e0;
         e[1] = e1;
         e[2] = e2;
+    }
+
+    static vec3 random() {
+        return vec3(random_double(), random_double(), random_double());
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
     }
 
 
@@ -107,6 +112,26 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
+}
+
+inline vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1,1);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
+inline vec3 random_unit_vector() {
+    return unit_vector(random_in_unit_sphere());
+}
+
+inline vec3 random_on_hemisphere(const vec3& normal) {
+    vec3 on_unit_sphere = random_unit_vector();
+    if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
+        return on_unit_sphere;
+    else
+        return -on_unit_sphere;
 }
 
 #endif

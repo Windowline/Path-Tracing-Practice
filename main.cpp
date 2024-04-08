4ce38b77b490712f1c891b58caab4f59233bd6db
박창선 <>
2024-04-08 11:42:07 +0900

    camera basis

--- main.cpp
+++ main.cpp
@@ -23,21 +23,16 @@ vec3 ray_color(const ray& r, const hittable& world) {
 int main() {
     hittable_list world;
 
-//    auto material_ground = std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0));
-//    auto material_right  = std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 1.0);
-//    auto material_center = make_shared<dielectric>(1.5);
-//    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_ground = make_shared<lambertian>(vec3(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(vec3(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0);
+    auto material_ground = std::make_shared<lambertian>(vec3(0.8, 0.8, 0.0));
+    auto material_center = std::make_shared<lambertian>(vec3(0.1, 0.2, 0.5));
+    auto material_left   = std::make_shared<dielectric>(1.5);
+    auto material_right  = std::make_shared<metal>(vec3(0.8, 0.6, 0.2), 0.0);
 
-
-    world.add(std::make_shared<sphere>(vec3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(std::make_shared<sphere>(vec3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(std::make_shared<sphere>(vec3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(std::make_shared<sphere>(vec3(-1.0,    0.0, -1.0),  -0.4, material_left));
-    world.add(std::make_shared<sphere>(vec3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(make_shared<sphere>(vec3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(vec3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(vec3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(vec3(-1.0,    0.0, -1.0),  -0.4, material_left));
+    world.add(make_shared<sphere>(vec3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam;
 
@@ -45,6 +40,11 @@ int main() {
     cam.image_width  = 400;
     cam.samples_per_pixel = 50;
     cam.max_depth = 50;
+
+    cam.vfov     = 20;
+    cam.lookfrom = vec3(-2,2,1);
+    cam.lookat   = vec3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
 
     cam.render(world);
 }
